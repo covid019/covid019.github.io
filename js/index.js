a7220fc8430ed10d2e5b480b66803d866e8f5684
ald3rs0n <aldersone213@gmail.com>
2020-03-31 04:35:38 +0530

    commit#8

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const body = document.getElementById("main")
 const table = document.createElement("table")
-
-body.innerHTML = "Hello World!"
-
+const date = document.createElement("p")
+date.innerHTML = new Date().toDateString()
+body.appendChild(date)
 function generateTableHead(table, data) {
     let thead = table.createTHead();
     table.className = "table"
@@ -10,22 +10,28 @@ function generateTableHead(table, data) {
     thead.className = "thead-dark"
     let row = thead.insertRow();
     for (let key in data) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
+        if(key == 1){
+            continue;
+        }
+        let th = document.createElement("th");
+        let text = document.createTextNode(data[key]);
+        th.appendChild(text);
+        row.appendChild(th);
     }
 }
 
 function generateTable(table, data) {
-    let tHeadData = Object.keys(data.Countries[0]);
+    let tHeadData = Object.keys(data[0]);
     generateTableHead(table,tHeadData);
     let tbody = document.createElement("tbody");
     tbody.className = "table-dark"
     table.appendChild(tbody)
-    for (let element of data.Countries) {
+    for (let element of data) {
     let row = tbody.insertRow();
         for (key in element) {
+            if(key == "Slug" || element["TotalConfirmed"] == 0){
+                continue;
+            }
             let cell = row.insertCell();
             let text = document.createTextNode(element[key]);
             cell.appendChild(text);
@@ -33,12 +39,17 @@ function generateTable(table, data) {
     }
 }
 
+function sortData(table,data){
+    let arr = Object.values(data.Countries)
+    let arr2 = arr.slice(0)
+            .sort((a,b)  => {return a.TotalConfirmed - b.TotalConfirmed})
+            .reverse()
+    generateTable(table,arr2)
+}
 fetch("https://api.covid19api.com/summary")
     .then((resp) => resp.json())
     .then(data => {
-        generateTable(table, data);
-
-        console.log((Object(data.Countries[1])))
+        sortData(table, data);
     })
     .catch(err => console.log(err))
 
